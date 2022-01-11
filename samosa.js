d15fd07ba9cc4da8e68af27541f2e06999ecdcc4
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-01-11 06:54:24 +0530

    Changed code

---
+++ samosa.js
@@ -0,0 +1,10 @@
+/* Reading Values in input boxes */
+var unchange = () => {
+    availQuantity = document.querySelector("#show-1").value;
+    quantitySold = document.querySelector("#show-2").value;
+}
+var change = () => {
+    perCost = document.querySelector("#perCost").value;
+    required = document.querySelector("#required").value;
+}
+/* Showing info by DHTML */
